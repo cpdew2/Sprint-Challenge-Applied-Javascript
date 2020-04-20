95e14b8f78fbb6e40fd69542d06aa7b93ad460d0
Cassandra Dew <cpdew2@gmail.com>
2020-04-19 20:23:02 -0400

    style assignments

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,31 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function article(input){
+
+    //elements
+const card = document.createElement('div');
+const headline = document.createElement('div');
+const author = document.createElement('div');
+const imgContainer = document.createElement('div');
+const img = document.createElement('img');
+const name = document.createElement('span');
+
+// assign element values
+
+headline.textContent = input.headline;
+img.src = input.authorPhoto;
+name.textContent = input.authorName;
+
+//assigning style
+
+card.classList.add('card');
+headline.classList.add('headline');
+author.classList.add('author');
+imgContainer.classList.add('img-container');
+
+//nesting elements
+
+
+}
